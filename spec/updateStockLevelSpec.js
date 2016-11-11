ee7bdc1275a00445b34f9a0f23b64b8253063ea6
Kris Quigley <kris@krisquigley.co.uk>
2016-11-11 00:08:17 +0000

    Allow 409 errors to be resolved

---
+++ spec/updateStockLevelSpec.js
@@ -0,0 +1,79 @@
+"use strict";
+const request        = require('request');
+const requestOptions = require('../requestOptions.js');
+const handleResponse = require('../handleResponse.js');
+
+exports.process = processAction;
+
+function processAction(message, config) {
+  let self = this;
+
+  // submits the HTTP request to the VMS to acknowledge the order
+  function acknowledgeSubOrder(authenticationHeaders) {
+    let { id } = message.body.currentMessage;
+    let { quantity } = message.body.cassellie.getStockLevel;
+
+    let options = Object.assign({}, authenticationHeaders, {
+      url: `${authenticationHeaders.url}sub-orders/${id}`,
+      json: true,
+      body: { data: { type: 'variants', id: id, attributes: { acknowledge: true } } }
+    });
+    console.log(options);
+
+    return new Promise((resolve, reject) => {
+      request.patch(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+
+  function handleError(response) {
+    if (response.statusCode == 408 || response.statusCode == 429 || response.statusCode == 503 || response.statusCode == 504) {
+      return emitRebound(Error(`Retrying...\nResponse status code was ${response.statusCode}`));
+    } else {
+      return emitRebound(Error(`Response status code was ${response.statusCode} not 2XX/3XX, body:\n${response.body}`));
+    }
+  }
+
+  function emitData(response) {
+    let body = response.body;
+
+    message.body.currentMessage = body;
+    if (message.body.vms == undefined) {
+      message.body.vms = {};
+    }
+    message.body.vms.acknowledgeSubOrder = { vmsSubOrderAcknowledgement: body };
+
+    console.log('Emitting data: ', message);
+    self.emit('data', message);
+    return Promise.resolve(message);
+  }
+
+  function emitError(error) {
+    console.log('An error occurred:', error);
+    self.emit('error', error);
+    return Promise.resolve();
+  }
+
+  function emitRebound(error) {
+    console.log('An error occurred:', error);
+    self.emit('rebound', error);
+    return Promise.resolve();
+  }
+
+  function emitEnd() {
+    console.log('Finished execution');
+    self.emit('end');
+  }
+
+  requestOptions(config).
+    then(acknowledgeSubOrder).
+    then(handleResponse).
+    then(emitData).
+    catch(handleError).
+    then(emitEnd);
+}
