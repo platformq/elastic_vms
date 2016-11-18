c5a2ee2e01dd9f0d09aa3637a8715e4bde803d71
Hubert Pompecki <hpompecki@gmail.com>
2016-11-18 15:41:08 +0000

    Allow action to receive data from vendorsForLeadTimes field

--- spec/getVendorsByReferenceSpec.js
+++ spec/getVendorsByReferenceSpec.js
@@ -2,6 +2,7 @@
 
 const getVendorsByReference = require('../lib/actions/getVendorsByReference.js');
 const orderWebhook = require('./fixtures/orderWebhook.json');
+const inputCustomField = require('./fixtures/getVendorsByReference/inputCustomField.json');
 const getVendorsByReferenceResponse = require('./fixtures/getVendorsResponse.json');
 const filterLeadTimes  = require('../lib/getVendorsByReference/filterLeadTimes.js');
 // disable requests to the outside world
@@ -14,25 +15,25 @@ console.log = () => {};
 describe("Retrieve vendors lead times", () => {
 
   beforeEach(() => {
-    this.message = {
-      body: orderWebhook
-    }
-
     this.config = {
       host:   "https://vendors-staging.herokuapp.com",
       apiKey: "abc123"
     }
   });
 
-  describe("Valid request", () => {
+  describe("Valid request with vendors in webhook data", () => {
     beforeEach((done) => {
-      this.getVendorsByReferenceRequest = nock('https://vendors-staging.herokuapp.com:443', 
+      this.getVendorsByReferenceRequest = nock('https://vendors-staging.herokuapp.com:443',
                                           {"encodedQueryParams":true})
                                       .get('/api/v1/admin/vendors?filter[references]=Ultra');
 
       this.self = {
         emit() { done(); }
       };
+
+      this.message = {
+        body: orderWebhook
+      }
 
       spyOn(this.self, "emit").and.callThrough();
 
@@ -52,7 +53,7 @@ describe("Retrieve vendors lead times", () => {
       let leadTimes = filterLeadTimes.filterLeadTimes({ body: getVendorsByReferenceResponse });
 
       expect(passedMessageVerb).toEqual('data');
-      expect(passedMessageBody).toEqual({ "webhook": orderWebhook.webhook, 
+      expect(passedMessageBody).toEqual({ "webhook": orderWebhook.webhook,
                                           "shopify": { packageNumbers: { ITEM0000: 1, ITEM0001: 3, ITEM0002: 5 } },
                                           "currentMessage": leadTimes,
                                           "vms": {
@@ -64,18 +65,58 @@ describe("Retrieve vendors lead times", () => {
     });
   });
 
+  describe("Valid request with vendors in custom field", () => {
+    beforeEach((done) => {
+      this.getVendorsByReferenceRequest = nock('https://vendors-staging.herokuapp.com:443',
+                                          {"encodedQueryParams":true})
+                                      .get('/api/v1/admin/vendors?filter[references]=Ultra');
+
+      this.self = {
+        emit() { done(); }
+      };
+
+      this.message = {
+        body: inputCustomField
+      }
+
+      spyOn(this.self, "emit").and.callThrough();
+
+      this.getVendorsByReferenceRequest = this.getVendorsByReferenceRequest.reply(200, getVendorsByReferenceResponse);
+
+      getVendorsByReference.process.call(this.self, this.message, this.config);
+    });
+
+    it("sends a correct request to a correct VMS endpoint", () => {
+      expect(this.getVendorsByReferenceRequest.isDone()).toBe(true);
+    });
+
+    it("emits valid JSON API compliant data", () => {
+      expect(this.self.emit).toHaveBeenCalledTimes(1);
+      let passedMessageVerb = this.self.emit.calls.argsFor(0)[0];
+      let passedMessageBody = this.self.emit.calls.argsFor(0)[1].body;
+      let leadTimes = filterLeadTimes.filterLeadTimes({ body: getVendorsByReferenceResponse });
+
+      expect(passedMessageVerb).toEqual('data');
+      expect(passedMessageBody.vms).toEqual({
+        "getVendorsByReference": {
+          "leadTimes": leadTimes
+        }
+      });
+    });
+  });
+
   describe("Invalid request", () => {
     beforeEach((done) => {
       this.self = {
         emit() { done(); }
       };
 
-      this.getVendorsByReferenceRequest = nock('https://vendors-staging.herokuapp.com:443', 
+      this.getVendorsByReferenceRequest = nock('https://vendors-staging.herokuapp.com:443',
                                           {"encodedQueryParams":true})
                                       .get('/api/v1/admin/vendors?filter[references]=undefined')
 
       this.message = {
-        body: { 
+        body: {
           webhook: {
             included: ["this is invalid data and will raise a rebound"]
           }
