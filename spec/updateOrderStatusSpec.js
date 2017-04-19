0bbbacd0e19cf1370b4a5af6dc0f44323223444d
Hubert Pompecki <hpompecki@gmail.com>
2017-04-19 15:42:03 +0100

    Update orders and returns

--- spec/updateOrderStatusSpec.js
+++ spec/updateOrderStatusSpec.js
@@ -5,16 +5,19 @@ const nock = require('nock');
 
 nock.disableNetConnect();
 
-// console.log = () => {};
+console.log = () => {};
 
 // fixtures
 const input = require('./fixtures/updateOrderStatus/input.json');
 const filterOrdersResponse = require('./fixtures/updateOrderStatus/filterOrdersResponse.json');
 const filterOrdersEmptyResponse = require('./fixtures/updateOrderStatus/filterOrdersEmptyResponse.json');
+const filterReturnsResponse = require('./fixtures/updateOrderStatus/filterReturnsResponse.json');
 const updateOrderRequest = require('./fixtures/updateOrderStatus/updateOrderRequest.json');
+const updateReturnRequest = require('./fixtures/updateOrderStatus/updateReturnRequest.json');
 const updateOrderResponse = require('./fixtures/updateOrderStatus/updateOrderResponse.json');
+const updateReturnResponse = require('./fixtures/updateOrderStatus/updateReturnResponse.json');
 
-describe("Updating order status", () => {
+describe("Updating order and return statuses", () => {
   beforeEach(() => {
     this.cfg = {
       host:   "https://vendors-staging.herokuapp.com",
@@ -22,7 +25,7 @@ describe("Updating order status", () => {
     };
   });
 
-  describe("Successful update", () => {
+  describe("Successful order update", () => {
     beforeEach((done) => {
       this.self = {
         emit(action) { if (action == 'end') done(); }
@@ -36,6 +39,10 @@ describe("Updating order status", () => {
       this.filterOrdersRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
           .get('/api/v1/orders?filter[consignment_reference]=EC-000-00A-K9G')
           .reply(200, filterOrdersResponse);
+
+      this.filterReturnsRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .get('/api/v1/admin/returns?filter[dms_reference]=EC-000-00A-K9G')
+          .reply(200, {data: []});
 
       this.updateOrderRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
           .patch('/api/v1/orders/123', updateOrderRequest)
@@ -62,7 +69,51 @@ describe("Updating order status", () => {
     });
   });
 
-  describe("Failed update", () => {
+  describe("Successful return update", () => {
+    beforeEach((done) => {
+      this.self = {
+        emit(action) { if (action == 'end') done(); }
+      };
+      spyOn(this.self, "emit").and.callThrough();
+
+      this.msg = {
+        body: input
+      };
+
+      this.filterOrdersRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .get('/api/v1/orders?filter[consignment_reference]=EC-000-00A-K9G')
+          .reply(200, {data: []});
+
+      this.filterReturnsRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .get('/api/v1/admin/returns?filter[dms_reference]=EC-000-00A-K9G')
+          .reply(200, filterReturnsResponse);
+
+      this.updateOrderRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .patch('/api/v1/admin/returns/51', updateReturnRequest)
+          .reply(200, updateReturnResponse);
+
+      updateOrderStatus.call(this.self, this.msg, this.cfg);
+    });
+
+    it("Fetches orders by consignment reference", () => {
+      expect(this.filterOrdersRequest.isDone()).toEqual(true);
+    });
+
+    it("Update the order status", () => {
+      expect(this.updateOrderRequest.isDone()).toEqual(true);
+    });
+
+    it("Emits data", () => {
+      expect(this.self.emit.calls.argsFor(0)[0]).toEqual('data');
+    });
+
+    it("Emits end", () => {
+      expect(this.self.emit).toHaveBeenCalledTimes(2);
+      expect(this.self.emit).toHaveBeenCalledWith('end');
+    });
+  });
+
+  describe("Failed order update", () => {
     beforeEach((done) => {
       this.self = {
         emit(action) { if (action == 'end') done(); }
@@ -76,6 +127,10 @@ describe("Updating order status", () => {
       this.filterOrdersRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
           .get('/api/v1/orders?filter[consignment_reference]=EC-000-00A-K9G')
           .reply(200, filterOrdersResponse);
+
+      this.filterReturnsRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .get('/api/v1/admin/returns?filter[dms_reference]=EC-000-00A-K9G')
+          .reply(200, {data: []});
 
       this.updateOrderRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
           .patch('/api/v1/orders/123', updateOrderRequest)
@@ -95,7 +150,7 @@ describe("Updating order status", () => {
     });
   });
 
-  describe("When the order does not exist", () => {
+  describe("When order and return do not exist", () => {
     beforeEach((done) => {
       this.self = {
         emit(action) { if (action == 'end') done(); }
@@ -109,6 +164,10 @@ describe("Updating order status", () => {
       this.filterOrdersRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
           .get('/api/v1/orders?filter[consignment_reference]=EC-000-00A-K9G')
           .reply(200, filterOrdersEmptyResponse);
+
+      this.filterReturnsRequest = nock('https://vendors-staging.herokuapp.com', {"encodedQueryParams":true})
+          .get('/api/v1/admin/returns?filter[dms_reference]=EC-000-00A-K9G')
+          .reply(200, {data: []});
 
       updateOrderStatus.call(this.self, this.msg, this.cfg);
     });
